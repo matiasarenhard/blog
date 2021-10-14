e98a7c43247af3d106005ef01fd3db305cf594d5
Matias <matias_arenhard@hotmail.com>
2021-10-13 21:44:30 -0300

    add simple css to components

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,11 @@
+import "./styles.css";
+import mainImagge from "./header.jpg"
+
 function Header(){
   return(
     <header>
         <h1>React Blog test</h1>
+        <img src={mainImagge} alt="only test"></img>
     </header>
   );
 }
